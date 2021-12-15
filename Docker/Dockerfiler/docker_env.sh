d4c4da0a55ab4fbb4422af1c862b8cdfcb58228c
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-12-15 17:21:43 +0100

    Update docker_env.sh

--- Docker/Dockerfiler/docker_env.sh
+++ Docker/Dockerfiler/docker_env.sh
@@ -1,5 +1,5 @@
 winpty docker run --name tomcat -it --rm -p 8888:8080 -d tomcat
-winpty docker run --name sonar --restart always -p 9000:9000 -d sonarqube
+winpty docker run --name sonar -it --restart always -p 9000:9000 -d sonarqube
 winpty docker run --name nexus -it --rm -p 8081:8081 -d sonatype/nexus3
 winpty docker run --name selenium_hub -it --rm -p 4441:4441 -d selenium/hub:4.1.0-20211209
 winpty docker run --name selenium_node_firefox -it --rm -p 4442:4442 -d selenium/standalone-firefox:4.1.0-20211209
