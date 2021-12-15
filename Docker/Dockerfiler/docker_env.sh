01e6abacc327d0f8c44efb07e0718f29bc39e45c
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-12-15 17:20:03 +0100

    Create docker_env.sh

---
+++ Docker/Dockerfiler/docker_env.sh
@@ -0,0 +1,7 @@
+winpty docker run --name tomcat -it --rm -p 8888:8080 -d tomcat
+winpty docker run --name sonar --restart always -p 9000:9000 -d sonarqube
+winpty docker run --name nexus -it --rm -p 8081:8081 -d sonatype/nexus3
+winpty docker run --name selenium_hub -it --rm -p 4441:4441 -d selenium/hub:4.1.0-20211209
+winpty docker run --name selenium_node_firefox -it --rm -p 4442:4442 -d selenium/standalone-firefox:4.1.0-20211209
+winpty docker run --name selenium_node_chrom -it --rm -p 4444:4444 -d selenium/standalone-chrome:4.1.0-20211209
+winpty docker run --name Jenkins -it --rm  -p 8080:8080 -p 50000:50000 -d jenkins/jenkins
