7e094c711da7da03d739ecc1b7e204c16e4e67c0
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-12-16 11:40:43 +0100

    Update docker_env.sh

--- Docker/Dockerfiler/docker_env.sh
+++ Docker/Dockerfiler/docker_env.sh
@@ -5,6 +5,7 @@ winpty docker run --name selenium_hub -it --rm -p 4441:4441 -d selenium/hub:4.1.
 winpty docker run --name selenium_node_firefox -it --rm -p 4442:4442 -d selenium/standalone-firefox:4.1.0-20211209
 winpty docker run --name selenium_node_chrom -it --rm -p 4444:4444 -d selenium/standalone-chrome:4.1.0-20211209
 winpty docker run --name Jenkins -it --rm  -p 8080:8080 -p 50000:50000 -d -v /c/Users/.../app/jenkins:/var/jenkins_home jenkins/jenkins
+#8080 to manage Jenkins, 50000 for builds.
 docker network create -d bridge docker_env_network
 winpty docker run -it --rm --net docker_env_network --name dockernginx -d nginx
 
